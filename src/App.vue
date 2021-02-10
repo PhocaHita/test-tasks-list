463926ea041b3a888eb4260cf9a06fdd174dc7db
phoca <posthita@gmail.com>
2021-02-10 21:56:11 +0300

    wip: first page html + css

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+.main-container
+    template(v-if="taskList.length")
+        .tasks-list(v-for="task in taskList")
+            .item
+                TaskItem(:item="task")
+
+    .tasks-menu
+        .task-actions
+            .button
+                .task-button.-add Add
+
+</template>
+
+<script>
+import TaskItem from './components/TaskItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskItem
+  },
+  data(){
+      return {
+          taskList: []
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+    .main-continer{
+        max-width: 1200px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 50px 0;
+    }
+    .tasks-list{
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0 -10px -20px;
+        & > .item{
+            flex-basis: 25%;
+            width: 100%;
+            padding: 0 10px;
+            margin-bottom: 20px;
+        }
+    }
+    .tasks-menu{
+        margin-top: 50px;
+        &:first-child{
+            margin-top: 0;
+        }
+    }
+</style>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,73 @@
+<template lang="pug">
+.task-item
+    .field {{item.id}}
+    .field {{item.name}}
+    .field {{item.description}}
+    .field {{item.status}}
+    .field {{item.date}}
+    .task-actions
+        .button
+            .task-button.-change Change
+        .button
+            .task-button.-delete Delete
+</template>
+
+<script>
+    export default {
+        name: 'TaskItem',
+        props: {
+            item: Object
+        }
+    }
+</script>
+
+<style lang="scss">
+    .tasks-item{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        overflow: hidden;
+        box-shadow: 0 5px 10px rgba(0,0,0, .5);
+        & > .field{
+            margin-bottom: 10px;
+        }
+    }
+
+    .task-actions{
+        display: flex;
+        align-items: center;
+        margin: 0 -10px;
+        & > .button{
+            flex-basis: 50%;
+            width: 100%;
+            padding: 0 10px;
+        }
+    }
+
+    .task-button{
+        width: 100%;
+        max-width: 200px;
+        margin: 0 auto;
+        text-align: center;
+        border-radius: 50px;
+        padding: 5px;
+        font-weight: 500;
+        cursor: pointer;
+        transition: all .2s ease-in-out;
+        &:hover{
+            opacity: .8;
+        }
+        &.-change{
+            color: black;
+            background-color: darkgoldenrod;
+        }
+        &.-delete{
+            color: white;
+            background-color: maroon;
+        }
+        &.-add{
+            color: white;
+            background-color: darkolivegreen;
+        }
+    }
+</style>
